750d90de1ad719d798dbe22d976303c054a224ce
Sam Deane <sam@elegantchaos.com>
2021-03-22 23:16:41 +0000

    Use 'contained' rather than 'location' as the context for contents

--- Sources/Lilliput/Behaviours/PlayerBehaviour.swift
+++ Sources/Lilliput/Behaviours/PlayerBehaviour.swift
@@ -113,7 +113,7 @@ struct PlayerBehaviour: Behaviour {
 
         for location in locations {
             // description of contents
-            output += location.describeContents(context: .location)
+            output += location.describeContents(context: .contained)
             
             // optional extra descriptions when certain objects are missing
             for string in location.definition.strings {
