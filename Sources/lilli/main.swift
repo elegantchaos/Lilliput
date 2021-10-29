e3aeee2f7b83b281a0423ac3793fd94865f48447
Sam Deane <sam@elegantchaos.com>
2021-10-29 12:14:13 +0100

    Pass game and commands to lilli

--- Sources/lilli/main.swift
+++ Sources/lilli/main.swift
@@ -7,17 +7,18 @@ import Foundation
 import Lilliput
 import LilliputExamples
 
-let driver = BasicDriver()
-let engine = Engine(driver: driver)
-let url = LilliputExamples.urlForGame(named: "PersonTest")!
-engine.load(url: url)
-
-if CommandLine.arguments.count > 1 {
-    let script = CommandLine.arguments[1]
-    if let url = LilliputExamples.script(named: script), FileManager.default.fileExists(atURL: url) {
-        engine.readScript(from: url)
-    }
+let args = CommandLine.arguments
+guard args.count > 1 else {
+    fatalError("Usage: \(args[0]) <game> {<commands>}")
 }
 
-
-engine.run()
+let driver = BasicDriver()
+let engine = Engine(driver: driver)
+let game = args[1]
+if let url = LilliputExamples.urlForGame(named: game) {
+    engine.load(url: url)
+    if let url = LilliputExamples.script(named: args.count > 2 ? args[2] : game) {
+        engine.readScript(from: url)
+    }
+    engine.run()
+}
