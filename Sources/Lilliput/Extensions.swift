5bed6d68a77e15c8eb53fc271475789b3ec0ee78
Sam Deane <sam@elegantchaos.com>
2022-04-04 15:04:25 +0100

    Removed things that have moved to CollectionExtensions

--- Sources/Lilliput/Extensions.swift
+++ Sources/Lilliput/Extensions.swift
@@ -11,21 +11,6 @@ func ???<T, P>(_ p: Optional<T>, t: (T) -> P?) -> P? {
     return p.flatMap(t)
 }
 
-public protocol InitMappable {
-    associatedtype FromType
-    init(_ from: FromType)
-}
-
-extension Collection {
-    public func map<T>(as type: T.Type) -> [T]  where T: InitMappable, T.FromType == Element {
-        self.map({ T($0) })
-    }
-}
-
-extension String: InitMappable {
-    public typealias FromType = Substring
-}
-
 extension String {
     var sentenceCased: String {
         guard let first = self.first else { return self }
