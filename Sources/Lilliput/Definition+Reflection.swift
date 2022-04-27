5a91b7dacfc2818fa30e3c6e13a21ec5094dc9e1
Sam Deane <sam@elegantchaos.com>
2022-04-27 15:10:17 +0100

    More reflection

--- Sources/Lilliput/Definition+Reflection.swift
+++ Sources/Lilliput/Definition+Reflection.swift
@@ -6,7 +6,29 @@
 import Foundation
 
 public extension Definition {
+    static let labels = [
+        "strings.definite": "Definite Name",
+        "strings.indefinite": "Indefinite Name",
+        "strings.location": "Location Description",
+        "strings.detailed": "Object Description"
+    ]
+
     var generalDescriptionKeys: [String] {
         return ["indefinite", "definite", "detailed"]
+    }
+    
+    func label(forPath path: String) -> String {
+
+        return Self.labels[path] ?? path
+    }
+    
+    func pathIsMultiline(_ path: String) -> Bool {
+        switch path {
+            case "strings.detailed", "strings.location":
+                return true
+                
+            default:
+                return false
+        }
     }
 }
